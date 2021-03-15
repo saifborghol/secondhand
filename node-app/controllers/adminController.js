6d2e10f2aca32141d7276623d99a36c7f6b611a4
Aziz <azizmdk@outlook.com>
2021-03-15 15:00:49 +0100

    change

--- node-app/controllers/adminController.js
+++ node-app/controllers/adminController.js
@@ -1,5 +1,9 @@
 const adminModel = require('../models/adminModel');
+const jwt = require('jsonwebtoken');
+const randtoken = require('rand-token');
 const bcrypt = require('bcrypt');
+const refreshTokens = [];
+const _ = require('lodash');
 
 module.exports = {
 	createAdmin: function (req, res) {
@@ -20,5 +24,63 @@ module.exports = {
 					data: admin,
 				});
 		});
+	},
+
+	authentificateAdmin: function (req, res, next) {
+		adminModel.findOne(
+			{ userName: req.body.userName },
+			function (err, userInfo) {
+				if (err || !userInfo) {
+					next(err);
+				} else {
+					if (bcrypt.compareSync(req.body.password, userInfo.password)) {
+						const token = jwt.sign(
+							{
+								id: userInfo._id,
+							},
+							req.app.get('secretKey'),
+							{ expiresIn: '2h' }
+						);
+						var refreshToken = randtoken.uid(256);
+						refreshTokens[refreshToken] = userInfo._id;
+						res.json({
+							status: 'Success',
+							message: 'Admin found!',
+							data: {
+								user: userInfo,
+								token: token,
+								refreshtoken: refreshToken,
+							},
+						});
+					} else {
+						res.json({
+							status: 'Error',
+							message: 'Invalid email/password!',
+							data: null,
+						});
+					}
+				}
+			}
+		);
+	},
+
+	logoutAdmin: function logout(req, res, next) {
+		var refreshToken = req.body.refreshToken;
+		console.log('refreshTokens : ', refreshTokens);
+		if (refreshToken in refreshTokens) {
+			delete refreshTokens[refreshToken];
+			console.log('refreshTokens : ', refreshTokens);
+			res.json({
+				status: 'success',
+				message: 'Logout with success refreshtoken: ',
+				data: refreshToken,
+			});
+		} else {
+			res.json({
+				status: 'error',
+				message: 'Logout Failed ',
+				data: null,
+			});
+		}
 	},
 };
