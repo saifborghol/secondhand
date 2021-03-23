0b1fc2c7e9a6c1a9c018d12ea691ef009bd4691f
Aziz <azizmdk@outlook.com>
2021-03-23 14:13:05 +0100

    change

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://localhost:27017/test",
+mongoose.connect("mongodb://localhost:27017/database",
 { useNewUrlParser: true, useUnifiedTopology: true }, (err)=>{
     if(err)
      {
