b305e988579b4c24f46ded01e6fa5de0af118ff8
Aziz <azizmdk@outlook.com>
2021-04-12 10:45:38 +0100

    change

--- front-end/src/components/pages/deposerannonce.jsx
+++ front-end/src/components/pages/deposerannonce.jsx
@@ -1,191 +1,190 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import Select from "@material-ui/core/Select";
-import InputLabel from "@material-ui/core/InputLabel";
+import Select from '@material-ui/core/Select';
 
-import Breadcrumb from "../common/breadcrumb";
+import Breadcrumb from '../common/breadcrumb';
 
-import ImageUploader from "react-images-upload";
+import ImageUploader from 'react-images-upload';
 
-import UserController from "../../services/controllers/userControllers";
-import CategoryController from "../../services/controllers/CategoryController";
-import SubCategoryController from "../../services/controllers/SubCategoryController";
+import UserController from '../../services/controllers/userControllers';
+import CategoryController from '../../services/controllers/CategoryController';
+import SubCategoryController from '../../services/controllers/SubCategoryController';
 
 class Deposer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      Categories: [],
-      SubCategories: [],
-      pictures: [],
-    };
-    this.UserController = new UserController();
-    this.CategoryController = new CategoryController();
-    this.SubCategoryController = new SubCategoryController();
+	constructor(props) {
+		super(props);
+		this.state = {
+			Categories: [],
+			SubCategories: [],
+			pictures: [],
+		};
+		this.UserController = new UserController();
+		this.CategoryController = new CategoryController();
+		this.SubCategoryController = new SubCategoryController();
 
-    this.onDrop = this.onDrop.bind(this);
-  }
+		this.onDrop = this.onDrop.bind(this);
+	}
 
-  componentDidMount() {
-    this.getAllCategories();
-    this.getAllSubCategories();
-  }
+	componentDidMount() {
+		this.getAllCategories();
+		this.getAllSubCategories();
+	}
 
-  getAllCategories() {
-    this.CategoryController.getAllCategory().then((res) => {
-      this.setState({ Categories: res.data.data });
-    });
-  }
+	getAllCategories() {
+		this.CategoryController.getAllCategory().then((res) => {
+			this.setState({ Categories: res.data.data });
+		});
+	}
 
-  getAllSubCategories() {
-    this.SubCategoryController.getAllSubCategory().then((res) => {
-      this.setState({ SubCategories: res.data.data });
-    });
-  }
+	getAllSubCategories() {
+		this.SubCategoryController.getAllSubCategory().then((res) => {
+			this.setState({ SubCategories: res.data.data });
+		});
+	}
 
-  onDrop(pictureFiles, pictureDataURLs) {
-    this.setState({
-      pictures: pictureFiles,
-    });
-  }
+	onDrop(pictureFiles, pictureDataURLs) {
+		this.setState({
+			pictures: pictureFiles,
+		});
+	}
 
-  handleSubmit = () => {};
+	handleSubmit = () => {};
 
-  render() {
-    return (
-      <div>
-        <Breadcrumb title={"Annonce"} />
+	render() {
+		return (
+			<div>
+				<Breadcrumb title={'Annonce'} />
 
-        {/*Regsiter section*/}
-        <section className="register-page section-b-space">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <h3>Déposer votre annonce</h3>
-                <div className="theme-card">
-                  <form className="theme-form">
-                    <div className="form-row">
-                      <div className="col-md-6">
-                        <label>Titre</label>
-                        <input
-                          name="name"
-                          type="text"
-                          className="form-control"
-                          id="fname"
-                          placeholder="Titre"
-                        />
-                        <label
-                          style={{
-                            paddingBottom: "20px",
-                            fontSize: 12,
-                            color: "red",
-                          }}
-                        />
-                      </div>
+				{/*Regsiter section*/}
+				<section className="register-page section-b-space">
+					<div className="container">
+						<div className="row">
+							<div className="col-lg-12">
+								<h3>Déposer votre annonce</h3>
+								<div className="theme-card">
+									<form className="theme-form">
+										<div className="form-row">
+											<div className="col-md-6">
+												<label>Titre</label>
+												<input
+													name="name"
+													type="text"
+													className="form-control"
+													id="fname"
+													placeholder="Titre"
+												/>
+												<label
+													style={{
+														paddingBottom: '20px',
+														fontSize: 12,
+														color: 'red',
+													}}
+												/>
+											</div>
 
-                      <div className="col-md-6">
-                        <label htmlFor="review">Prix</label>
-                        <input
-                          name="surName"
-                          type="text"
-                          className="form-control"
-                          id="lname"
-                          placeholder="Prix"
-                        />
-                        <label
-                          style={{
-                            paddingBottom: "20px",
-                            fontSize: 12,
-                            color: "red",
-                            marginTop: -20,
-                          }}
-                        />
-                      </div>
-                    </div>
-                    <div className="form-row">
-                      <div className="col-md-6">
-                        <label htmlFor="email">Description (optionnelle)</label>
-                        <br />
+											<div className="col-md-6">
+												<label htmlFor="review">Prix</label>
+												<input
+													name="surName"
+													type="text"
+													className="form-control"
+													id="lname"
+													placeholder="Prix"
+												/>
+												<label
+													style={{
+														paddingBottom: '20px',
+														fontSize: 12,
+														color: 'red',
+														marginTop: -20,
+													}}
+												/>
+											</div>
+										</div>
+										<div className="form-row">
+											<div className="col-md-6">
+												<label htmlFor="email">Description (optionnelle)</label>
+												<br />
 
-                        <textarea
-                          className="form-control"
-                          style={{ resize: "none" }}
-                          name="Description"
-                          rows="5"
-                          cols="93"
-                        />
+												<textarea
+													className="form-control"
+													style={{ resize: 'none' }}
+													name="Description"
+													rows="5"
+													cols="93"
+												/>
 
-                        <label
-                          style={{
-                            paddingBottom: "20px",
-                            fontSize: 12,
-                            color: "red",
-                          }}
-                        />
-                      </div>
-                      <div className="col-md-6">
-                        <label htmlFor="review">Catégorie</label>
-                        <br />
-                        <Select native defaultValue="Choisir une catégorie">
-                          <option
-                            aria-label="None"
-                            value="Choisir une catégorie"
-                          />
-                          {this.state.Categories.map((cat, index) => {
-                            return (
-                              <React.Fragment>
-                                <optgroup label={cat.title}>
-                                  {this.state.Categories[index].subcat.map(
-                                    (subcat) => (
-                                      <option value={subcat._id}>
-                                        {subcat.title}
-                                      </option>
-                                    )
-                                  )}
-                                </optgroup>
-                              </React.Fragment>
-                            );
-                          })}
-                        </Select>
-                        <label
-                          style={{
-                            paddingBottom: "20px",
-                            fontSize: 12,
-                            color: "red",
-                          }}
-                        />
-                        <br /> <br /> <br />
-                        <br />
-                        <label for="avatar">selectionner vos images</label>
-                        <ImageUploader
-                          withIcon={true}
-                          withPreview
-                          buttonText="Choisir vos images"
-                          onChange={this.onDrop}
-                          imgExtension={[".jpg", ".gif", ".png", ".gif"]}
-                          maxFileSize={5242880}
-                          withLabel={false}
-                          fileSizeError="La taille des fichiers est trop large"
-                          fileTypeError="Le type de fichiers n'est pas supporté"
-                        />
-                      </div>
+												<label
+													style={{
+														paddingBottom: '20px',
+														fontSize: 12,
+														color: 'red',
+													}}
+												/>
+											</div>
+											<div className="col-md-6">
+												<label htmlFor="review">Catégorie</label>
+												<br />
+												<Select native>
+													<option
+														aria-label="None"
+														value="Choisir une catégorie"
+													/>
+													{this.state.Categories.map((cat, index) => {
+														return (
+															<React.Fragment>
+																<optgroup label={cat.title}>
+																	{this.state.Categories[index].subcat.map(
+																		(subcat) => (
+																			<option value={subcat._id}>
+																				{subcat.title}
+																			</option>
+																		)
+																	)}
+																</optgroup>
+															</React.Fragment>
+														);
+													})}
+												</Select>
+												<label
+													style={{
+														paddingBottom: '20px',
+														fontSize: 12,
+														color: 'red',
+													}}
+												/>
+												<br /> <br /> <br />
+												<br />
+												<label for="avatar">selectionner vos images</label>
+												<ImageUploader
+													withIcon={true}
+													withPreview
+													buttonText="Choisir vos images"
+													onChange={this.onDrop}
+													imgExtension={['.jpg', '.gif', '.png', '.gif']}
+													maxFileSize={5242880}
+													withLabel={false}
+													fileSizeError="La taille des fichiers est trop large"
+													fileTypeError="Le type de fichiers n'est pas supporté"
+												/>
+											</div>
 
-                      <a
-                        className="btn btn-solid"
-                        onClick={() => this.handleSubmit()}
-                      >
-                        Déposer
-                      </a>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>
-    );
-  }
+											<a
+												className="btn btn-solid"
+												onClick={() => this.handleSubmit()}
+											>
+												Déposer
+											</a>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+		);
+	}
 }
 
 export default Deposer;
