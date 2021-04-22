6688fd56356ea5408d11f71b3320828ca3fc2c31
Aziz <azizmdk@outlook.com>
2021-04-22 10:30:25 +0100

    c

--- front-end/src/components/layouts/common/products.jsx
+++ front-end/src/components/layouts/common/products.jsx
@@ -1,104 +1,54 @@
-import React, { Component } from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import { ProductCard } from 'react-ui-cards';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import CardGroup from 'react-bootstrap/CardGroup';
-import CardDeck from 'react-bootstrap/CardDeck';
-import axios from 'axios';
-import AnnonceController from '../../../services/controllers/AnnonceController';
+import React, { Component } from "react";
+
+import Card from "react-bootstrap/Card";
+
+import AnnonceController from "../../../services/controllers/AnnonceController";
 
 export default class SpecialProducts extends Component {
-	constructor() {
-		super();
-		this.state = {
-			PROD: [],
-		};
-		this.AnnonceController = new AnnonceController();
-	}
+  constructor() {
+    super();
+    this.state = {
+      PROD: [],
+    };
+    this.AnnonceController = new AnnonceController();
+  }
 
-	componentDidMount() {
-		this.AnnonceController.getAll().then((res) => {
-			this.setState({ PROD: res.data.data });
-			console.log('PRODUCT_DATA', this.state.PROD);
-		});
-	}
-	render() {
-		const tab = ['https://i.imgur.com/im6SfJP.png'];
-		return (
-			<div>
-				{/* <CardDeck>
-					{this.state.PROD.map((annonce) => {
-						return (
-							<Card>
-								<Card.Img
-									variant="top"
-									src={`http://localhost:4000/annonce/annonceimage/${
-										annonce.product.image
-									}`}
-								/>
-								<Card.Body>
-									<Card.Title>{annonce.title}</Card.Title>
-									<Card.Text>
-										This is a wider card with supporting text below as a natural
-										lead-in to additional content. This content is a little bit
-										longer.
-									</Card.Text>
-								</Card.Body>
-								<Card.Footer>
-									<small className="text-muted">{annonce.date}</small>
-								</Card.Footer>
-							</Card>
-						);
-					})}
-				</CardDeck>
-
-				<CardGroup style={{ display: 'flex', flexWrap: 'wrap' }}>
-					{this.state.PROD.map((annonce) => {
-						return (
-							<Card style={{ width: '18rem', margin: '20px' }}>
-								<Card.Img
-									variant="top"
-									src={`http://localhost:4000/annonce/${annonce.product.image}`}
-								/>
-								<Card.Body>
-									<Card.Title>Card Title</Card.Title>
-									<Card.Text>
-										Some quick example text to build on the card title and make
-										up the bulk of the card's content.
-									</Card.Text>
-								</Card.Body>
-							</Card>
-						);
-					})}
-				</CardGroup> */}
-				<div id="card-container">
-					{this.state.PROD.map((annonce) => {
-						console.log('IMAGES:', annonce.image[0].name);
-						return (
-							<Card id="card-style">
-								<Card.Img
-									style={{ width: '240px', height: '135px' }}
-									variant="top"
-									src={`http://localhost:4000/annonce/annonceImage/${
-										annonce.image[0].name
-									}`}
-								/>
-								<Card.Body>
-									<Card.Title style={{ fontWeight: '700' }}>
-										{annonce.title}
-									</Card.Title>
-									<Card.Title>{annonce.price} DT</Card.Title>
-									<Card.Text>
-										Publié par: {annonce.user_id.name} {annonce.user_id.surName}
-									</Card.Text>
-									<Card.Text>{annonce.date}</Card.Text>
-								</Card.Body>
-							</Card>
-						);
-					})}
-				</div>
-			</div>
-		);
-	}
+  componentDidMount() {
+    this.AnnonceController.getAll().then((res) => {
+      this.setState({ PROD: res.data.data });
+      console.log("PRODUCT_DATA", this.state.PROD);
+    });
+  }
+  render() {
+    const tab = ["https://i.imgur.com/im6SfJP.png"];
+    return (
+      <div>
+        <div id="card-container">
+          {this.state.PROD.map((annonce) => {
+            return (
+              <Card id="card-style">
+                <Card.Img
+                  style={{ width: "240px", height: "135px" }}
+                  variant="top"
+                  src={`http://localhost:4000/annonce/annonceImage/${
+                    annonce.image[0].name
+                  }`}
+                />
+                <Card.Body>
+                  <Card.Title style={{ fontWeight: "700" }}>
+                    {annonce.title}
+                  </Card.Title>
+                  <Card.Title>{annonce.price} DT</Card.Title>
+                  <Card.Text>
+                    Publié par: {annonce.user_id.name} {annonce.user_id.surName}
+                  </Card.Text>
+                  <Card.Text>{annonce.date}</Card.Text>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
--- front-end/src/components/pages/profil.jsx
+++ front-end/src/components/pages/profil.jsx
@@ -1,171 +1,192 @@
-import React, { Component } from 'react';
-import { Helmet } from 'react-helmet';
-import Breadcrumb from '../common/breadcrumb';
-import NewProduct from '../common/new-product';
-import Filter from '../collection/common/filter';
-import FilterBar from '../collection/common/filter-bar';
-import ProductListing from '../collection/common/product-listing';
-import StickyBox from 'react-sticky-box';
+import React, { Component } from "react";
+import { Helmet } from "react-helmet";
+import Breadcrumb from "../common/breadcrumb";
+import NewProduct from "../common/new-product";
+import Filter from "../collection/common/filter";
+import FilterBar from "../collection/common/filter-bar";
+import ProductListing from "../collection/common/product-listing";
+import StickyBox from "react-sticky-box";
+import Card from "react-bootstrap/Card";
 
-import { FaEdit } from 'react-icons/fa';
+import { FaEdit } from "react-icons/fa";
 
-import avatar from '../../assets/images/default-avatar.png';
+import avatar from "../../assets/images/default-avatar.png";
 
-import userController from '../../services/controllers/userControllers';
+import userController from "../../services/controllers/userControllers";
 
 class Profil extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			layoutColumns: 3,
-			User: {},
-		};
-		this.userController = new userController();
-	}
+    this.state = {
+      layoutColumns: 3,
+      User: {},
+      Annonce: [],
+    };
+    this.userController = new userController();
+  }
 
-	componentDidMount() {
-		if (localStorage.getItem('userId')) {
-			this.getUser(localStorage.getItem('userId'));
-		}
-	}
+  componentDidMount() {
+    if (localStorage.getItem("userId")) {
+      this.getUser(localStorage.getItem("userId"));
+    }
+  }
 
-	componentDidUpdate() {
-		if (localStorage.getItem('userId')) {
-			this.getUser(localStorage.getItem('userId'));
-		}
-	}
+  componentDidUpdate() {
+    if (localStorage.getItem("userId")) {
+      this.getUser(localStorage.getItem("userId"));
+    }
+  }
 
-	LayoutViewClicked(colums) {
-		this.setState({
-			layoutColumns: colums,
-		});
-	}
+  LayoutViewClicked(colums) {
+    this.setState({
+      layoutColumns: colums,
+    });
+  }
 
-	openFilter = () => {
-		document.querySelector('.collection-filter').style = 'left: -15px';
-	};
+  openFilter = () => {
+    document.querySelector(".collection-filter").style = "left: -15px";
+  };
 
-	getUser(id) {
-		this.userController.getUser(id).then((res) => {
-			this.setState({ User: res.data.data });
-		});
-	}
+  getUser(id) {
+    this.userController.getUser(id).then((res) => {
+      this.setState({ User: res.data.data });
+      this.setState({ Annonce: res.data.data.annonce });
+    });
+  }
 
-	editProfil = () => {
-		const { history } = this.props;
-		history.push('/editprofile');
-	};
+  editProfil = () => {
+    const { history } = this.props;
+    history.push("/editprofile");
+  };
 
-	render() {
-		return (
-			<div>
-				{/*SEO Support*/}
-				<Helmet>
-					<title>secondhand | Achat et vente en ligne</title>
-				</Helmet>
-				{/*SEO Support End */}
+  render() {
+    return (
+      <div>
+        {/*SEO Support*/}
+        <Helmet>
+          <title>secondhand | Achat et vente en ligne</title>
+        </Helmet>
+        {/*SEO Support End */}
 
-				<Breadcrumb title={'Profile'} />
+        <Breadcrumb title={"Profile"} />
 
-				<section className="section-b-space">
-					<div className="collection-wrapper">
-						<div className="container">
-							<div className="row">
-								<div className="collection-content col">
-									<div className="page-main-content ">
-										<div className="">
-											<div className="row">
-												<div className="col-sm-12">
-													<div className="top-banner-wrapper">
-														{/* <a href="#">
-															<img
-																src={`${
-																	process.env.PUBLIC_URL
-																}/assets/images/mega-menu/2.jpg`}
-																className="img-fluid"
-																alt=""
-															/>
-														</a> */}
-														<div className="profil-page">
-															{this.state.User.image ? (
-																<img
-																	src={`http://localhost:4000/user/userimage/${
-																		this.state.User.image
-																	}`}
-																	alt="avatar"
-																	className="girl"
-																/>
-															) : (
-																<img
-																	src={avatar}
-																	alt="avatar"
-																	className="girl"
-																/>
-															)}
-															<h3>
-																{this.state.User.name +
-																	' ' +
-																	this.state.User.surName}
-																<FaEdit
-																	size="20"
-																	style={{
-																		cursor: 'pointer',
-																		marginLeft: '10px',
-																	}}
-																	onClick={() => this.editProfil()}
-																/>
-															</h3>
-														</div>
-													</div>
-													<div className="collection-product-wrapper">
-														<div className="product-top-filter">
-															<div className="container-fluid p-0">
-																<div className="row">
-																	<div className="col-xl-12">
-																		<div className="filter-main-btn">
-																			<span
-																				onClick={this.openFilter}
-																				className="filter-btn btn btn-theme"
-																			>
-																				<i
-																					className="fa fa-filter"
-																					aria-hidden="true"
-																				/>{' '}
-																				Filter
-																			</span>
-																		</div>
-																	</div>
-																</div>
-																<div className="row">
-																	<div className="col-12">
-																		<FilterBar
-																			onLayoutViewClicked={(colmuns) =>
-																				this.LayoutViewClicked(colmuns)
-																			}
-																		/>
-																	</div>
-																</div>
-															</div>
-														</div>
+        <section className="section-b-space">
+          <div className="collection-wrapper">
+            <div className="container">
+              <div className="row">
+                <div className="collection-content col">
+                  <div className="page-main-content ">
+                    <div className="">
+                      <div className="row">
+                        <div className="col-sm-12">
+                          <div className="top-banner-wrapper">
+                            <div className="profil-page">
+                              {this.state.User.image ? (
+                                <img
+                                  src={`http://localhost:4000/user/userimage/${
+                                    this.state.User.image
+                                  }`}
+                                  alt="avatar"
+                                  className="girl"
+                                />
+                              ) : (
+                                <img
+                                  src={avatar}
+                                  alt="avatar"
+                                  className="girl"
+                                />
+                              )}
+                              <h3>
+                                {this.state.User.name +
+                                  " " +
+                                  this.state.User.surName}
+                                <FaEdit
+                                  size="20"
+                                  style={{
+                                    cursor: "pointer",
+                                    marginLeft: "10px",
+                                  }}
+                                  onClick={() => this.editProfil()}
+                                />
+                              </h3>
+                            </div>
+                          </div>
+                          <div className="collection-product-wrapper">
+                            <div className="product-top-filter">
+                              <div className="container-fluid p-0">
+                                <div className="row">
+                                  <div className="col-xl-12">
+                                    <div className="filter-main-btn">
+                                      <span
+                                        onClick={this.openFilter}
+                                        className="filter-btn btn btn-theme"
+                                      >
+                                        <i
+                                          className="fa fa-filter"
+                                          aria-hidden="true"
+                                        />{" "}
+                                        Filter
+                                      </span>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="row">
+                                  <div className="col-12">
+                                    <FilterBar
+                                      onLayoutViewClicked={(colmuns) =>
+                                        this.LayoutViewClicked(colmuns)
+                                      }
+                                    />
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
 
-														{/*Products Listing Component*/}
-														<ProductListing
-															colSize={this.state.layoutColumns}
-														/>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
-			</div>
-		);
-	}
+                            <div id="card-container">
+                              {this.state.Annonce.map((annonce) => {
+                                return (
+                                  <Card id="card-style">
+                                    <Card.Img
+                                      style={{
+                                        width: "240px",
+                                        height: "135px",
+                                      }}
+                                      variant="top"
+                                      src={`http://localhost:4000/annonce/annonceImage/${
+                                        annonce.image[0].name
+                                      }`}
+                                    />
+                                    <Card.Body>
+                                      <Card.Title style={{ fontWeight: "700" }}>
+                                        {annonce.title}
+                                      </Card.Title>
+                                      <Card.Title>
+                                        {annonce.price} DT
+                                      </Card.Title>
+                                      <Card.Text>
+                                        Publié par: {annonce.user_id.name}{" "}
+                                        {annonce.user_id.surName}
+                                      </Card.Text>
+                                      <Card.Text>{annonce.date}</Card.Text>
+                                    </Card.Body>
+                                  </Card>
+                                );
+                              })}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Profil;
